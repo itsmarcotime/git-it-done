30508d2418ec471c9e7e69eee67034313f702e08
Marco Mata <patrickstar1281@yahoo.com>
2022-06-07 04:41:34 -0500

    finished with list-issues stuff

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
 
@@ -12,7 +13,14 @@ var getRepoIssues = function(repo) {
 
                 //pass response data to dom function
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+
             });
+
         } else {
             alert("There was a problem with your request!");
         }
@@ -64,6 +72,21 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
 
     }
+
+};
+
+var displayWarning = function(repo) {
+
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
 
 };
 
